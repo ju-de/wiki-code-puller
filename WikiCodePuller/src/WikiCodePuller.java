3b8990ce0cb629d6b99880b148a0d480cb531f9b
Judy Chen <judy.chen@uwaterloo.ca>
2015-05-27 15:24:01 -0400

    remove tags from strings

--- WikiCodePuller/src/WikiCodePuller.java
+++ WikiCodePuller/src/WikiCodePuller.java
@@ -9,27 +9,36 @@ public class WikiCodePuller{
 		String line = "";
 		File file = new File("xss.txt");
 
+		//read file
 		try{
 			Scanner sc = new Scanner(file);
 			
 			while(sc.hasNextLine()){
 				line = line + sc.nextLine();
 			}
-			
+			sc.close();
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 
 		//System.out.println(line);
 
-		
+		//extract code snippets
 		Pattern p = Pattern.compile("<pre>(.+?)</pre>");
 		Matcher m = p.matcher(line);
 		List<String> xss = new ArrayList<String>();
 
 		while(m.find()){
 			xss.add(m.group());
-			System.out.println(m.group());
+		}
+		
+		for(int i = 0; i<xss.size(); i++){
+			xss.set(i, xss.get(i).replaceAll("<pre>",""));
+			xss.set(i, xss.get(i).replaceAll("</pre>",""));
+		}
+		
+		for(int i = 0; i<xss.size(); i++){
+			System.out.println(xss.get(i));
 		}
 		
 	}
